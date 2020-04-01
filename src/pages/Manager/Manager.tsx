70d9e2f0b072c08ad0a3a641a45e7e03ef652852
mac <mac@macs-MacBook-Pro.local>
2020-04-01 13:33:35 +0800

    Charts added

--- src/pages/Manager/Manager.tsx
+++ src/pages/Manager/Manager.tsx
@@ -4,8 +4,8 @@ import { Theme } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/styles';
 import { SearchBar } from '../../components/SearchBar';
 import { TeamTableWrapper } from '../../components/TeamTable';
-// import { Chart } from '../../components/Manager';
-// import { ChartLocation } from '../../components/Charts/ChartLocation';
+import { ChartStatus } from '../../components/Charts/ChartStatus';
+import { ChartLocation } from '../../components/Charts/ChartLocation';
 
 export function Manager() {
   const classes = useStyles();
@@ -24,24 +24,24 @@ export function Manager() {
       <TeamTableWrapper />
     </Grid>
   )
-//   const pageTitle = 'Your Team\'s Overview';
-//     const diagram1 = 'Team\'s Status';
-//     const diagram2 = 'Work Location';
-//         return (
-//             <div style={{ display: 'flex', alignContent: 'center', flexDirection: 'column', marginTop: '2vh', marginBottom: '3vh'}}>
-//                 <p style={{ textAlign: 'center'}}>{pageTitle}</p>
-//                 <div className='diagramContainer' style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-//                     <div className='teamStatus' style={{ margin: '2vh 2vw', height: '50vh', width: '25vw'}}>
-//                         <div style={{ textAlign: 'center', margin: '2vh 2vw'}}>{diagram1}</div>
-//                         <Chart />
-//                     </div>
-//                     <div className='workLocation' style={{ margin: '2vh 2vw', height: '50vh', width: '25vw'}}>
-//                         <div style={{ textAlign: 'center', margin: '2vh 2vw'}}>{diagram2}</div>
-//                         <ChartLocation />
-//                     </div>
-//                 </div>
-//             </div>
-//         );
+  // const pageTitle = 'Your Team\'s Overview';
+  //   const diagram1 = 'Team\'s Status';
+  //   const diagram2 = 'Work Location';
+  //       return (
+  //           <div style={{ display: 'flex', alignContent: 'center', flexDirection: 'column', marginTop: '2vh', marginBottom: '3vh'}}>
+  //               <p style={{ textAlign: 'center'}}>{pageTitle}</p>
+  //               <div className='diagramContainer' style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+  //                   <div className='teamStatus' style={{ margin: '2vh 2vw', height: '50vh', width: '25vw'}}>
+  //                       <div style={{ textAlign: 'center', margin: '2vh 2vw'}}>{diagram1}</div>
+  //                       <ChartStatus />
+  //                   </div>
+  //                   <div className='workLocation' style={{ margin: '2vh 2vw', height: '50vh', width: '25vw'}}>
+  //                       <div style={{ textAlign: 'center', margin: '2vh 2vw'}}>{diagram2}</div>
+  //                       <ChartLocation />
+  //                   </div>
+  //               </div>
+  //           </div>
+  //       );
 }
 
 const useStyles = makeStyles((theme: Theme) => ({
